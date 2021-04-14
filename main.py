0ee3c1785130cd0587ea0fa5efbd8da4a827dd1e
Nail Minnigalin <nail160306@gmail.com>
2021-04-14 21:27:02 +0300

    Complete is_palindrome

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 def is_palindrome(s):
-    pass
+    s = s.lower()  # Noon is palindrome but N != n.
+    for i in range(len(s) // 2):  # Check first half of string
+        if not s[i] == s[len(s) - i - 1]:  # Check if element i from behind == element i from end
+            return False
+    return True  # If we here word is palindrome
